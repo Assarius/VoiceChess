bc0974e2b7965d28737384c244c3a161005a43ac
Assarius <mskrok899@gmail.com>
2022-10-22 17:46:08 +0200

    Adaptation of menu.py file to work properly

--- voicechess/menu.py
+++ voicechess/menu.py
@@ -1,21 +1,24 @@
 import pygame
 import pygame_menu
+import game
 
 
 class Menu:
+    def __init__(self, WIN):
+        self.WIN = WIN
 
-    def start_game_single():
-        Game.main(1)
+    def start_game_single(self):
+        game.Game.main(players=1, WIN=self.WIN)
 
-    def start_game_multi():
-        Game.main(2)      
+    def start_game_multi(self):
+        game.Game.main(players=2, WIN=self.WIN)
 
 
-    def draw_menu(WIN):
+    def draw_menu(self):
         pygame.init()
-        menu = pygame_menu.Menu('VoiceChess', WIN.get_width(), WIN.get_height(), 
+        menu = pygame_menu.Menu('VoiceChess', self.WIN.get_width(), self.WIN.get_height(), 
                                 theme=pygame_menu.themes.THEME_BLUE)
-        menu.add.button('SINGLEPLAYER', Menu.start_game_single)
-        menu.add.button('MULTIPLAYER', Menu.start_game_multi)
+        menu.add.button('SINGLEPLAYER', Menu.start_game_single, self)
+        menu.add.button('MULTIPLAYER', Menu.start_game_multi, self)
         menu.add.button('WYJDŹ', pygame_menu.events.EXIT)
-        menu.mainloop(WIN)
+        menu.mainloop(self.WIN)
