d1ea20a09fcddf18d93f81cd7beb43b86c8b9991
Assarius <mskrok899@gmail.com>
2022-11-11 22:35:59 +0100

    Remove unnecessary code

--- voicechess/game.py
+++ voicechess/game.py
@@ -9,24 +9,19 @@ import player
 
 class Game:
     def main(players, WIN):
-        
         BOARD = board.Board(WIN)
         clock = pygame.time.Clock()
         run = True
         hist = [sunfish.Position(sunfish.initial, 0, (True,True), (True,True), 0, 0)]
         searcher = compressed.Searcher()
         hist_moves = []
-
         while run:
             clock.tick(60)
             pygame.event.pump()
-                # if event.type == pygame.QUIT:
-                #     exit()
             while True:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         exit()
-
                 BOARD.draw_pieces(BOARD, hist[-1], hist_moves) 
                 pygame.display.update()
                 pygame.event.poll()
